d17d559854730b6de314d0495b639a6285d4f081
Ric Charles Paquibot <125810674+ric2thiss@users.noreply.github.com>
2026-09-23 23:13:33 +0800

    Refine day13 layout and styling

--- day13/src/components/Header/Header.jsx
+++ day13/src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-
-import "./Header.css"
-import Logo from "../../assets/logo.png"
 import HeaderActions from '../HeaderActions/HeaderActions'
+import Logo from "../../assets/logo.png"
+import "./Header.css"
 
 function Header() {
   return (
-    <header>
-        <Navbar />
-        <img src={Logo} alt="Logo" />
-        <HeaderActions />
+    <header className="site-header">
+      <Navbar />
+
+      <a href="/" className="site-logo" aria-label="Home">
+        <img src={Logo} alt="" />
+      </a>
+
+      <HeaderActions />
     </header>
   )
 }
