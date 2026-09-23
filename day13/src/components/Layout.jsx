64f897b221bb414d717867ed20d17fb47cac4ccb
Ric Charles Paquibot <125810674+ric2thiss@users.noreply.github.com>
2026-09-23 09:13:08 +0800

    Apply background image to Day 13 layout

--- day13/src/components/Layout.jsx
+++ day13/src/components/Layout.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header/Header'
+import background from '../assets/background.png'
 
 function Layout() {
   return (
-    <>
-        <Header />
-
-        <Outlet />
-    </>
+    <div
+      className="main-container"
+      style={{
+        backgroundImage: `url(${background})`,
+        minHeight: '100dvh',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <Header />
+      <Outlet />
+    </div>
   )
 }
 
